12e6bce8f5691ca6ee567340dbf999d6b326ad8d
Daviddddl <997004866@qq.com>
2017-10-11 18:54:37 +0800

    Initial commit

---
+++ src/Compile.java
@@ -0,0 +1,10 @@
+public class Compile {
+
+    public static void main(String[] args){
+        /*创建词法分析类*/
+        TestLexer testLexer = new TestLexer("./src/input.txt");
+        //FileUtil.clearFile();//清空文件
+        testLexer.analyse();
+    }
+
+}
